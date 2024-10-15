afd3bc0b2b000c10059673392515fd29df015ec3
sameer dhulap <28100769+sameerdhulap@users.noreply.github.com>
2024-10-15 18:34:54 +0530

    remove native integration

---
+++ android/app/src/main/java/com/marketingconnector/batchconnector/MainActivity.java
@@ -0,0 +1,6 @@
+package com.marketingconnector.batchconnector;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/marketingconnector/batchconnector/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.marketingconnector.batchconnector
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
